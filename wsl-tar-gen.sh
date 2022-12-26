4757f06427d09b44ae406cfba9a9d302bf4921d7
Vaisakh Murali <mvaisakh@statixos.com>
2022-12-26 19:47:55 +0600

    wdt: initial tar generation script
    This will generate WSL importable tar files that are generated from
    existing docker container images.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ wsl-tar-gen.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Copyright (C) 2022 Vaisakh Murali
+# SPDX-License-Identifier: GPL-3.0-only
+
+# Dependency check
+if command -v docker > /dev/null 2>&1 ; then
+    echo "docker found"
+    echo "$(docker -v)"
+else
+    echo "docker not found! Please install docker and run this script again!"
+    exit
+fi
+
+if command -v awk > /dev/null 2>&1 ; then
+    echo "awk found"
+    echo "$(docker -v)"
+else
+    echo "awk not found! Please install awk and run this script again!"
+    exit
+fi
+
+# set dir for tars
+export TAR_DIR="$PWD"
+
+# parse desired distro
+DISTROS=(
+    "alpine"
+    "archlinux"
+    "centos"
+    "debian"
+    "fedora"
+    "ubuntu"
+)
+
+# begin tar generation
+for ELEMENT in ${DISTROS[@]}; do
+    [[ -d $ELEMENT ]] && docker run -t centos sh ls / || exit
+    containerID=$(docker container ls -a | grep -i $ELEMENT | awk '{print $1}') || exit
+    docker export $containerID > $TAR_DIR/$ELEMENT-$(date +%d%m%y).tar || exit
+    # Remove the docker image to avoid piling up of containers
+    docker rm $containerID
+done
